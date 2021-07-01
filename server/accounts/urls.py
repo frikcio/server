93351ebdc6c4ab48520da4f95d5104134a250531
frikcio <frikcio67@gmail.com>
2021-07-01 17:49:13 +0300

    changed imports

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,12 +1,9 @@
-from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordChangeView
-from django.contrib.auth.views import PasswordResetDoneView as ResetDone
-from django.contrib.auth.views import PasswordResetConfirmView as ResetConfirm
-from django.contrib.auth.views import PasswordResetCompleteView as ResetComplete
-from django.contrib.auth.views import PasswordChangeDoneView as ChangeDone
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordChangeView, \
+    PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView, PasswordChangeDoneView
 
 from django.urls import path
 
-from accounts.views import *
+from .views import *
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -15,16 +12,16 @@ urlpatterns = [
     path('profile/', UserUpdateView.as_view(), name='profile'),
     path('password/reset/', PasswordResetView.as_view(template_name='accounts/password_reset.html'),
          name='password_reset'),
-    path('reset/done/', ResetDone.as_view(template_name='accounts/password_reset_done.html'),
+    path('reset/done/', PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
          name='password_reset_done'),
-    path('reset/<str:uidb64>/<str:token>/', ResetConfirm.as_view(template_name='accounts/reset_confirm.html'),
+    path('reset/<str:uidb64>/<str:token>/', PasswordResetConfirmView.as_view(template_name='accounts/reset_confirm.html'),
          name='password_reset_confirm'),
     path('reset/complete/',
-         ResetComplete.as_view(template_name='accounts/reset_complete.html'),
+         PasswordResetCompleteView.as_view(template_name='accounts/reset_complete.html'),
          name='password_reset_complete'),
     path('settings/password/', PasswordChangeView.as_view(template_name='accounts/password_change.html'),
          name='password_change'),
-    path('settings/password/done/', ChangeDone.as_view(template_name='accounts/password_change_done.html'),
+    path('settings/password/done/', PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'),
          name='password_change_done'),
     path('activate/<str:uid64>/<str:token>/', AccountActivateView.as_view(template_name='accounts/register.html'),
          name='activate_user'),
