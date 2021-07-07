d54ee9eaeadfb7be63a31ad92aedb5e845f31420
frikcio <frikcio67@gmail.com>
2021-07-07 10:16:07 +0300

    Created ne model Config

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -2,13 +2,19 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 
+from django.conf import settings
+
 
 class User(AbstractUser):
     avatar = models.ImageField(blank=True, null=True)
     email = models.EmailField(unique=True, blank=False, null=False)
-    confirm_email = models.BooleanField(default=False)
     birth_date = models.DateField(blank=True, null=True)
 
     @property
     def age(self):
         return int((timezone.now().date() - self.birth_date).year)
+
+
+class Config(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='config')
+    send_reminder_email = models.BooleanField(default=False)
