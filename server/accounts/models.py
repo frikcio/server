272070b92500d874adeebe320a9206e35eadabff
frikcio <frikcio67@gmail.com>
2021-07-13 18:28:15 +0300

    created class for create group, updated gender's choices

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,19 +1,26 @@
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
 from django.utils import timezone
 
-GENDER_CHOICES = (
-    (1, ("Not selected")),
-    (2, ("man")),
-    (3, ("woman")),
-)
+
+class GroupCreation(object):
+    NAMES = ['readers', 'writers']
+
+    def create(self):
+        # Create users groups for server
+        Group.objects.bulk_create([Group(name=name) for name in self.NAMES])  # Create all groups from GROUP_NAMES
 
 
 class User(AbstractUser):
+    class Gender(models.IntegerChoices):
+        NOT_SELECTED = 0
+        MAN = 1
+        WOMAN = 2
+
     email = models.EmailField(unique=True, blank=False, null=False)
     birth_date = models.DateField(blank=True, null=True)
-    gender = models.IntegerField(choices=GENDER_CHOICES, default=1)
+    gender = models.IntegerField(choices=Gender.choices, default=Gender.NOT_SELECTED)
 
     def __str__(self):
         return self.username
