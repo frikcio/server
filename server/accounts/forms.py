57be56d0edfd0a87f86ecac752735ffd032bd6c5
frikcio <frikcio67@gmail.com>
2021-07-14 19:12:46 +0300

    replaced groups field

--- server/accounts/forms.py
+++ server/accounts/forms.py
@@ -1,21 +1,14 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import Group
 
+from .choices import GroupChoices
 from .models import Settings
-
-
-# Get qoups queryset
-groups_list = Group.objects.all()
-
-# Creating CHOICES, depend on groups count
-GROUP_CHOICES = ([(group.pk, group) for group in groups_list])
 
 
 class RegisterForm(UserCreationForm):
     email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
-    groups = forms.ChoiceField(choices=GROUP_CHOICES, required=True)
+    groups = forms.ChoiceField(choices=GroupChoices.choices, required=True)
 
     class Meta:
         model = get_user_model()
