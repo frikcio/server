36fc6e5042aaa9f0ab54701d0ee7a387e39e4e57
frikcio <frikcio67@gmail.com>
2021-07-14 19:23:37 +0300

    Append new test for user's group

--- server/accounts/tests/unit_tests/test_registration.py
+++ server/accounts/tests/unit_tests/test_registration.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import Group
 from faker import Factory
 
 from django.contrib.auth import get_user_model
@@ -15,10 +16,12 @@ class RegistrationTest(TestCase):
     def setUp(self):
         username = fake.name().split(" ")[0]  # fake.name() return "Name Surname", so I split the string and get "Name"
         password = fake.password()
+        self.group = Group.objects.first()
         self.data = {'username': username,
                      'email': fake.email(),
                      'password1': password,
                      'password2': password,
+                     'groups': self.group.pk,
                      }
         self.url = reverse('register')
 
@@ -33,3 +36,11 @@ class RegistrationTest(TestCase):
         self.client.post(self.url, self.data)
         user = User.objects.first()
         self.assertFalse(user.is_active)
+
+    def test_user_group(self):
+        # Check that user is contains in group
+        self.client.post(self.url, self.data)
+        another_group = Group.objects.last()
+        user = User.objects.first()
+        self.assertIn(self.group, user.groups.all())
+        self.assertNotIn(another_group, user.groups.all())
