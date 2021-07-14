928fc77617d7f2a7cb63eb15077c31c2d708353f
frikcio <frikcio67@gmail.com>
2021-07-14 19:21:51 +0300

    Replaced on UserFactory and appended a new test

--- server/accounts/tests/unit_tests/test_change_mailing_status.py
+++ server/accounts/tests/unit_tests/test_change_mailing_status.py
@@ -1,20 +1,13 @@
-from faker import Factory
-
-from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
+from accounts.factories import UserFactory
 from accounts.models import Settings
-
-User = get_user_model()
-
-fake = Factory.create()
 
 
 class ChangeMailingStatusTests(TestCase):
     def setUp(self):
-        username = fake.name().split(' ')[0]  # fake.name() return "Name Surname", so I split the string and get "Name"
-        self.user = User.objects.create_user(username=username, password=fake.password())
+        self.user = UserFactory()
         self.user_settings = Settings.objects.create(user=self.user)
         self.client.force_login(user=self.user)
         self.url = reverse('account_settings', kwargs={'user_pk': self.user.pk})
@@ -22,11 +15,15 @@ class ChangeMailingStatusTests(TestCase):
     def test_change_mailing_status(self):
         # Check that mailing status changed
         mailing_status_before = self.user_settings.periodic_mailing
-        self.client.post(self.url + '?status=true', **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
+        response = self.client.post(self.url + '?status=true', **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
         mailing_status_after = Settings.objects.get(user__pk=self.user.pk).periodic_mailing
         self.assertNotEqual(mailing_status_before, mailing_status_after)
-
-    def test_response_status_code(self):
-        # Check that status code
-        response = self.client.post(self.url + '?status=true', **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
         self.assertEqual(response.status_code, 202)
+
+    def test_url_not_have_params(self):
+        # Check that if url not have query params, return status_code 400
+        mailing_status_before = self.user_settings.periodic_mailing
+        response = self.client.post(self.url, **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
+        mailing_status_after = Settings.objects.get(user__pk=self.user.pk).periodic_mailing
+        self.assertEqual(mailing_status_before, mailing_status_after)
+        self.assertEqual(response.status_code, 400)
