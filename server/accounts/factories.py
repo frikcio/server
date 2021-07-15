3d9dfa9de5daa0c9654204d4229dc1a51d1a1b52
frikcio <frikcio67@gmail.com>
2021-07-15 19:14:42 +0300

    Changed getting permissions for model

--- server/accounts/factories.py
+++ server/accounts/factories.py
@@ -3,6 +3,7 @@ import factory
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
 
+
 User = get_user_model()
 
 
@@ -17,20 +18,18 @@ class UserFactory(factory.django.DjangoModelFactory):
     last_name = factory.Faker('last_name')
 
 
-class GroupPermissions(object):
-    @staticmethod
-    def add_readers_permissions(group_name):
-        post_list = Permission.objects.filter(codename__contains='post').exclude(codename__contains='delete')
-        read_board = Permission.objects.get(codename='view_board')
-        read_topic = Permission.objects.get(codename='view_topic')
-        group_name.permissions.set(post_list)
-        group_name.permissions.add(read_board, read_topic)
+def add_readers_permissions_to(group_name):
+    post_list = Permission.objects.filter(content_type__model='post').exclude(codename__contains='delete')
+    read_board = Permission.objects.get(codename='view_board')
+    read_topic = Permission.objects.get(codename='view_topic')
+    group_name.permissions.set(post_list)
+    group_name.permissions.add(read_board, read_topic)
 
-    @staticmethod
-    def add_writers_permissions(group_name):
-        board_list = Permission.objects.filter(codename__contains='board')
-        topic_list = Permission.objects.filter(codename__contains='topic').exclude(codename__contains='delete')
-        read_post = Permission.objects.get(codename='view_post')
-        writers_permissions = board_list | topic_list
-        group_name.permissions.set(writers_permissions)
-        group_name.permissions.add(read_post)
+
+def add_writers_permissions_to(group_name):
+    board_list = Permission.objects.filter(content_type__model='board')
+    topic_list = Permission.objects.filter(content_type__model='topic').exclude(codename__contains='delete')
+    read_post = Permission.objects.get(codename='view_post')
+    writers_permissions = board_list | topic_list
+    group_name.permissions.set(writers_permissions)
+    group_name.permissions.add(read_post)
