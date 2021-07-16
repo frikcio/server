2fce12939e6185b22f898fa17d5bdc029396571f
frikcio <frikcio67@gmail.com>
2021-07-16 15:51:24 +0300

    appended UserUpdate and Avatar forms

--- server/accounts/forms.py
+++ server/accounts/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 from .choices import GroupChoices
-from .models import Settings
+from .models import Settings, Avatar
 
 
 class RegisterForm(UserCreationForm):
@@ -19,3 +19,23 @@ class SettingsForm(forms.ModelForm):
     class Meta:
         model = Settings
         fields = ['periodic_mailing']
+
+
+class UserUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name', 'gender']
+
+
+class AvatarForm(forms.ModelForm):
+    x = forms.FloatField(widget=forms.HiddenInput())
+    y = forms.FloatField(widget=forms.HiddenInput())
+    width = forms.FloatField(widget=forms.HiddenInput())
+    height = forms.FloatField(widget=forms.HiddenInput())
+
+    class Meta:
+        model = Avatar
+        fields = ('avatar', 'x', 'y', 'width', 'height',)
