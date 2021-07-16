a7ee8c3f0f044bd1e4c368924d77ffb33e2d9ecd
frikcio <frikcio67@gmail.com>
2021-07-16 15:50:02 +0300

    Used content_type instead codename. Optimized request in db

--- server/accounts/factories.py
+++ server/accounts/factories.py
@@ -3,6 +3,8 @@ import factory
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
 
+from boards.models import Post, Topic, Board
+from django.db.models import Q
 
 User = get_user_model()
 
@@ -18,18 +20,22 @@ class UserFactory(factory.django.DjangoModelFactory):
     last_name = factory.Faker('last_name')
 
 
-def add_readers_permissions_to(group_name):
-    post_list = Permission.objects.filter(content_type__model='post').exclude(codename__contains='delete')
-    read_board = Permission.objects.get(codename='view_board')
-    read_topic = Permission.objects.get(codename='view_topic')
-    group_name.permissions.set(post_list)
-    group_name.permissions.add(read_board, read_topic)
+def add_readers_permissions_to_group(group_name):
+    board_lookup = Q(content_type__model=Post._meta.model_name)
+    topic_lookup = Q(content_type__model=Topic._meta.model_name)
+    post_permissions = Permission.objects.filter(content_type__model=Post._meta.model_name).exclude(
+        codename__contains='delete')
+    view_permissions = Permission.objects.filter(board_lookup | topic_lookup).filter(
+        codename__contains='view')
+    readers_permissions = post_permissions | view_permissions
+    group_name.permissions.set(readers_permissions)
 
 
-def add_writers_permissions_to(group_name):
-    board_list = Permission.objects.filter(content_type__model='board')
-    topic_list = Permission.objects.filter(content_type__model='topic').exclude(codename__contains='delete')
-    read_post = Permission.objects.get(codename='view_post')
-    writers_permissions = board_list | topic_list
+def add_writers_permissions_to_group(group_name):
+    board_permissions = Permission.objects.filter(content_type__model=Board._meta.model_name)
+    topic_permissions = Permission.objects.filter(content_type__model=Topic._meta.model_name).exclude(
+        codename__contains='delete')
+    view_post_permission = Permission.objects.filter(content_type__model=Post._meta.model_name).filter(
+        codename__contains='view')
+    writers_permissions = board_permissions | topic_permissions | view_post_permission
     group_name.permissions.set(writers_permissions)
-    group_name.permissions.add(read_post)
