32461b171b8238ea17c221e119b71fd0d09991f4
frikcio <frikcio67@gmail.com>
2021-07-19 19:09:50 +0300

    Deleted choices, age and Avatar model

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,35 +1,23 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
-from django.utils import timezone
+
+from .choices import Gender
 
 
 class User(AbstractUser):
-    class Gender(models.IntegerChoices):
-        NOT_SELECTED = 0
-        MAN = 1
-        WOMAN = 2
-
     email = models.EmailField(unique=True, blank=False, null=False)
     birth_date = models.DateField(blank=True, null=True)
-    gender = models.IntegerField(choices=Gender.choices, default=Gender.NOT_SELECTED)
+    gender = models.CharField(max_length=1, choices=Gender.choices, default=Gender.NOT_SELECTED)
+    avatar = models.ImageField(blank=True, null=True, upload_to=f'accounts/')
 
     def __str__(self):
         return self.username
 
-    @property
-    def age(self):
-        return int((timezone.now().date() - self.birth_date).year)
-
-
-class Avatar(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='avatar')
-    avatar = models.ImageField(blank=True, null=True, upload_to=f'accounts/{user}/')
+    def get_upload_path(self):
+        return f'accounts/{self.username}/avatar/'
 
 
 class Settings(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='settings')
     periodic_mailing = models.BooleanField(default=False)
-
-    def __str__(self):
-        return ".periodic_mailing"
