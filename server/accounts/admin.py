7cac46597c239d2c0c32f509b943db1237632100
frikcio <frikcio67@gmail.com>
2021-07-19 19:03:25 +0300

    deleted Avatar model

--- server/accounts/admin.py
+++ server/accounts/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
-from .models import Avatar
-
 User = get_user_model()
 
 admin.site.register(User)
-admin.site.register(Avatar)
