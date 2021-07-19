89ed17444785b4c9ed75048fce5a56b539b717e1
frikcio <frikcio67@gmail.com>
2021-07-19 19:04:16 +0300

    created text choices for user's gender

--- server/accounts/choices.py
+++ server/accounts/choices.py
@@ -6,3 +6,9 @@ from django.utils.translation import gettext_lazy as _
 class GroupChoices(models.TextChoices):
     READERS = 'readers', _('readers')
     WRITERS = 'writers', _('writers')
+
+
+class Gender(models.TextChoices):
+    NOT_SELECTED = 'u', _('unknown')
+    MAN = 'm', _('man')
+    WOMAN = 'w', _('woman')
