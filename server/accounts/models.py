619de6ad34dec68d8661ef45388fe256c7dcc956
frikcio <frikcio67@gmail.com>
2021-07-23 14:56:00 +0300

    Changed upload_to path and appended initials to user model

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -9,13 +9,24 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, blank=False, null=False)
     birth_date = models.DateField(blank=True, null=True)
     gender = models.CharField(max_length=1, choices=Gender.choices, default=Gender.NOT_SELECTED)
-    avatar = models.ImageField(blank=True, null=True, upload_to=f'accounts/')
+    avatar = models.ImageField(blank=True, null=True, upload_to='%Y/%m/%d/%H:%M:%S')
 
     def __str__(self):
         return self.username
 
-    def get_upload_path(self):
-        return f'accounts/{self.username}/avatar/'
+    @property
+    def initials(self):
+        initials = ''
+        full_name = self.get_full_name()
+        text = full_name.strip()
+        if text:
+            split_text = text.split(' ')
+            if len(split_text) > 1:
+                initials = split_text[0][0] + split_text[-1][0]
+            else:
+                initials = split_text[0][0]
+
+        return initials.upper()
 
 
 class Settings(models.Model):
