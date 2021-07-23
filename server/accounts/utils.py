0d678d9cffbf03ddc1e54b85045e92e4e5dca9af
frikcio <frikcio67@gmail.com>
2021-07-23 15:08:16 +0300

    optimized code

--- server/accounts/utils.py
+++ server/accounts/utils.py
@@ -1,4 +1,14 @@
 import re
+import random
+from xml.sax.saxutils import escape as xml_escape
+
+DEFAULT_COLORS = [
+    "#1abc9c", "#16a085", "#f1c40f", "#f39c12", "#2ecc71", "#27ae60",
+    "#e67e22", "#d35400", "#3498db", "#2980b9", "#e74c3c", "#c0392b",
+    "#9b59b6", "#8e44ad", "#bdc3c7", "#34495e", "#2c3e50", "#95a5a6",
+    "#7f8c8d", "#ec87bf", "#d870ad", "#f69785", "#9ba37e", "#b49255",
+    "#b49255", "#a94136",
+]
 
 INITIALS_SVG_TEMPLATE = """
     <svg xmlns="http://www.w3.org/2000/svg"
@@ -16,4 +26,15 @@ INITIALS_SVG_TEMPLATE = """
             style="font-weight: 400; font-size: 80px">{text}</text>
     </svg>
     """.strip()
+
 INITIALS_SVG_TEMPLATE = re.sub('(\s+|\n)', ' ', INITIALS_SVG_TEMPLATE)
+
+def get_svg_avatar(initials):
+    svg_avatar = INITIALS_SVG_TEMPLATE.format(**{
+        'color1': random.choice(DEFAULT_COLORS),
+        'color2': random.choice(DEFAULT_COLORS),
+        'text_color': random.choice(DEFAULT_COLORS),
+        'text': xml_escape(initials),
+    }).replace('\n', '')
+
+    return svg_avatar
