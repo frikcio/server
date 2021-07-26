c73675232047fbc0c013c161cd8491aaba82fd32
frikcio <frikcio67@gmail.com>
2021-07-26 18:27:10 +0300

    replaced str message on template message

--- server/accounts/tasks.py
+++ server/accounts/tasks.py
@@ -1,25 +1,25 @@
 import base64
 
 from celery import shared_task
+from django.conf import settings
 
 from django.core.mail import send_mass_mail
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
-
+from django.template.loader import render_to_string
 
 User = get_user_model()
 
 
 @shared_task
-def send_verification_email(user_pk, absolute_url):
+def send_verification_email(user_pk, http_host):
     # send verification email for user
     user = User.objects.get(pk=user_pk)
     token = default_token_generator.make_token(user)
     uid64 = base64.urlsafe_b64encode(str(user.pk).encode()).decode()
-    message = f'Hi {user}\n you just register in our site\n To continue registration please follow: \n' \
-              f'{absolute_url}activate/{uid64}/{token}/' \
-              f' to activate your account'
-    user.email_user(subject=user, message=message, from_email="develop Team")
+    message = render_to_string('accounts/verification_email.html',
+                               {'user': user.username, 'uid64': uid64, 'token': token, 'http_host': http_host})
+    user.email_user(subject=user, message=message, from_email=settings.EMAIL_SENDER)
 
 
 @shared_task
@@ -28,7 +28,7 @@ def distribute_emails():
     recipients = User.objects.filter(settings__periodic_mailing=True)
     emails = []
     for user in recipients:
-        message = f'Hi {user}\n you get this email because you accept periodic mailing'
-        email_data = (user, message, "develop@team.com", [user.email])
+        message = render_to_string('accounts/distribution_email.html', {'user': user })
+        email_data = (user, message, settings.EMAIL_SENDER, [user.email])
         emails.append(email_data)
     send_mass_mail(emails, fail_silently=True)
