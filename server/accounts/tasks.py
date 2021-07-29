abbdf5a4b566fbda6429392a5b19a0708196812b
frikcio <frikcio67@gmail.com>
2021-07-29 13:12:49 +0300

    Replaced user.email_user on send_email

--- server/accounts/tasks.py
+++ server/accounts/tasks.py
@@ -1,9 +1,9 @@
 import base64
 
 from celery import shared_task
-from django.conf import settings
 
-from django.core.mail import send_mass_mail
+from django.core.mail import send_mail, send_mass_mail
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.template.loader import render_to_string
@@ -19,7 +19,8 @@ def send_verification_email(user_pk, http_host):
     uid64 = base64.urlsafe_b64encode(str(user.pk).encode()).decode()
     message = render_to_string('accounts/verification_email.html',
                                {'user': user.username, 'uid64': uid64, 'token': token, 'http_host': http_host})
-    user.email_user(subject=user, message=message, from_email=settings.EMAIL_SENDER)
+    sender = settings.EMAIL_HOST_USER
+    send_mail(subject='Registration', message=message, from_email=sender, recipient_list=[user.email])
 
 
 @shared_task
@@ -27,8 +28,9 @@ def distribute_emails():
     # send emails only for willing users
     recipients = User.objects.filter(settings__periodic_mailing=True)
     emails = []
+    sender = settings.EMAIL_HOST_USER
     for user in recipients:
         message = render_to_string('accounts/distribution_email.html', {'user': user })
-        email_data = (user, message, settings.EMAIL_SENDER, [user.email])
+        email_data = (user, message, sender, [user.email])
         emails.append(email_data)
     send_mass_mail(emails, fail_silently=True)
